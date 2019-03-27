d33913f70e857334c3274ff4ead72b5df6b026f0
zyq <544741562>
2019-03-27 14:54:11 +0800

    redis ok

--- zyq-dubbo-redis/src/main/java/com/zyq/redis/RedisClient.java
+++ zyq-dubbo-redis/src/main/java/com/zyq/redis/RedisClient.java
@@ -1,11 +1,75 @@
 package com.zyq.redis;
 
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
 
 public class RedisClient {
-    public static void main(String[] args) {
-        Jedis jedis = new Jedis("203.195.157.208",6379);
-        jedis.set("zyq","niubi");
-        System.out.println(jedis.get("zyq"));
+
+    private final static String REDIS_URL = "203.195.157.208";
+
+    private final static int REDIS_PORT = 6397;
+
+    private static int MAX_ACTIVE = 1024;
+
+    //控制一个pool最多有多少个状态为idle(空闲的)的jedis实例，默认值也是8。
+    private static int MAX_IDLE = 200;
+
+    //等待可用连接的最大时间，单位毫秒，默认值为-1，表示永不超时。如果超过等待时间，则直接抛出JedisConnectionException
+    private static int MAX_WAIT = 10000;
+
+    //连接超时的时间　　
+    private static int TIMEOUT = 10000;
+
+    // 在borrow一个jedis实例时，是否提前进行validate操作；如果为true，则得到的jedis实例均是可用的；
+    private static boolean TEST_ON_BORROW = true;
+
+    private static JedisPool jedisPool = null;
+
+    /**
+     * 初始化redis连接池
+     * @param args
+     */
+    static {
+        try {
+            JedisPoolConfig config = new JedisPoolConfig();
+            config.setMaxTotal(MAX_ACTIVE);
+            config.setMaxIdle(MAX_IDLE);
+            config.setMaxWaitMillis(MAX_WAIT);
+            config.setTestOnBorrow(TEST_ON_BORROW);
+            jedisPool = new JedisPool(config, REDIS_URL, REDIS_PORT, TIMEOUT);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
+
+    /**
+     * 获取jedis实例
+     * @param
+     */
+    public synchronized static Jedis getJedis() {
+        try {
+            if (jedisPool != null) {
+                Jedis jedis = jedisPool.getResource();
+                return jedis;
+            } else {
+                return null;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
+    /**
+     * 释放资源
+     * @param args
+     */
+    public static void returnResource(final Jedis jedis) {
+        if (jedis != null) {
+            jedisPool.returnResource(jedis);
+        }
+    }
+
 }
