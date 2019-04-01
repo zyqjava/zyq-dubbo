4a89b84c01fe9a24dadadebd9a615343b848c6e5
zyq <544741562>
2019-04-01 15:14:02 +0800

    netty ok

--- zyq-dubbo-protocol/src/main/java/com/zyq/protocol/dubbo/NettyClientHandler.java
+++ zyq-dubbo-protocol/src/main/java/com/zyq/protocol/dubbo/NettyClientHandler.java
@@ -15,12 +15,19 @@ public class NettyClientHandler extends ChannelInboundHandlerAdapter implements 
     private String result;
 
 
+    /**
+     * 和服务器进行通信
+     * @param ctx
+     * @param msg
+     * @throws Exception
+     */
     @Override
     public synchronized void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         System.out.println("接收到数据" + msg);
         result = msg.toString();
         notify();
     }
+
 
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
@@ -32,6 +39,15 @@ public class NettyClientHandler extends ChannelInboundHandlerAdapter implements 
         this.invocation = invocation;
     }
 
+    /**
+     * 为什么要加synchronized?
+     * 因为wait和notify都需要和synchronized一起使用，否则会报IllegalMonitorStateException
+     * 就是说wait和notify都需要和对象锁一起来使用
+     * 为什么wait和notify要放在object中？
+     * 因为object是所有类的父类，所以为了可以任何类使用放在object
+     * @return
+     * @throws Exception
+     */
     @Override
     public synchronized Object call() throws Exception {
         context.writeAndFlush(this.invocation);
